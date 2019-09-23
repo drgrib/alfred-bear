a93924b537cad1783c6d6539c1d2a92443379481
Chris Redford <credford@gmail.com>
2019-09-22 20:33:47 -0700

    link

---
+++ cmd/link/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/drgrib/alfred-bear/db"
+)
+
+func main() {
+	noteID := os.Args[1]
+
+	litedb, err := db.NewBearDB()
+	if err != nil {
+		panic(err)
+	}
+
+	callback := fmt.Sprintf("bear://x-callback-url/open-note?id=%s&show_window=yes&new_window=yes", noteID)
+	rows, err := litedb.Query(fmt.Sprintf(db.NOTE_TITLE_BY_ID, noteID))
+	if err != nil {
+		panic(err)
+	}
+	title := rows[0][db.TitleKey]
+	link := fmt.Sprintf("[%s](%s)", title, callback)
+
+	fmt.Print(link)
+}
--- cmd/search/main.go
+++ cmd/search/main.go
@@ -66,13 +66,13 @@ func main() {
 
 	switch {
 	case strings.HasPrefix(lastElement, "#"):
-		rows, err := litedb.Query(fmt.Sprintf(TAGS_BY_TITLE, lastElement[1:]))
+		rows, err := litedb.Query(fmt.Sprintf(db.TAGS_BY_TITLE, lastElement[1:]))
 		if err != nil {
 			panic(err)
 		}
 
 		for _, row := range rows {
-			tag := "#" + row[TitleKey]
+			tag := "#" + row[db.TitleKey]
 			autocomplete := strings.Join(elements[:len(elements)-1], " ") + " " + tag + " "
 			alfred.Add(alfred.Item{
 				Title:        tag,
@@ -82,7 +82,7 @@ func main() {
 		}
 
 	case len(words) == 0 && len(tags) == 0 && lastElement == "":
-		rows, err := litedb.Query(RECENT_NOTES)
+		rows, err := litedb.Query(db.RECENT_NOTES)
 		if err != nil {
 			panic(err)
 		}
@@ -95,7 +95,7 @@ func main() {
 			tagConditions = append(tagConditions, c)
 		}
 		tagConjunction := strings.Join(tagConditions, " OR ")
-		rows, err := litedb.Query(fmt.Sprintf(NOTES_BY_TAGS_AND_QUERY, tagConjunction, wordStr, wordStr, len(tags), wordStr))
+		rows, err := litedb.Query(fmt.Sprintf(db.NOTES_BY_TAGS_AND_QUERY, tagConjunction, wordStr, wordStr, len(tags), wordStr))
 		if err != nil {
 			panic(err)
 		}
@@ -103,7 +103,7 @@ func main() {
 
 	default:
 
-		rows, err := litedb.Query(fmt.Sprintf(NOTES_BY_QUERY, wordStr, wordStr, wordStr))
+		rows, err := litedb.Query(fmt.Sprintf(db.NOTES_BY_QUERY, wordStr, wordStr, wordStr))
 		if err != nil {
 			panic(err)
 		}
