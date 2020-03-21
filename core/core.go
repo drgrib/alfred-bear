a9f833922bd2dea7d86b9a6d6082baa87ffdb95c
Chris Redford <credford@gmail.com>
2020-03-21 14:12:23 -0700

    escape

--- core/core.go
+++ core/core.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/atotto/clipboard"
 	"github.com/drgrib/alfred"
+	escape "github.com/tj/go-pg-escape"
 	"golang.org/x/text/unicode/norm"
 
 	"github.com/drgrib/alfred-bear/db"
@@ -55,55 +56,6 @@ type Query struct {
 	LastToken, WordString string
 }
 
-func escape(source string) string {
-	var j int = 0
-	if len(source) == 0 {
-		return ""
-	}
-	tempStr := source[:]
-	desc := make([]byte, len(tempStr)*2)
-	for i := 0; i < len(tempStr); i++ {
-		flag := false
-		var escape byte
-		switch tempStr[i] {
-		case '\r':
-			flag = true
-			escape = '\r'
-			break
-		case '\n':
-			flag = true
-			escape = '\n'
-			break
-		case '\\':
-			flag = true
-			escape = '\\'
-			break
-		case '\'':
-			flag = true
-			escape = '\''
-			break
-		case '"':
-			flag = true
-			escape = '"'
-			break
-		case '\032':
-			flag = true
-			escape = 'Z'
-			break
-		default:
-		}
-		if flag {
-			desc[j] = '\\'
-			desc[j+1] = escape
-			j = j + 2
-		} else {
-			desc[j] = tempStr[i]
-			j = j + 1
-		}
-	}
-	return string(desc[0:j])
-}
-
 func ParseQuery(arg string) Query {
 	query := Query{}
 	query.Tokens = strings.Split(norm.NFC.String(arg), " ")
@@ -119,7 +71,7 @@ func ParseQuery(arg string) Query {
 		}
 	}
 	query.LastToken = query.Tokens[len(query.Tokens)-1]
-	query.WordString = escape(strings.Join(words, " "))
+	query.WordString = escape.Escape(strings.Join(words, " "))
 	return query
 }
 
